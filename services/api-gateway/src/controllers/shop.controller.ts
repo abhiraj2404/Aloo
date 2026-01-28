1940d12144076588dc3d855bdac57f49cf5786a3
Abhiraj Singh Chauhan <chauhanabhiraj06@gmail.com>
2026-01-28 17:04:16 +0530

    fix: internal package dependency resolution

--- services/api-gateway/src/controllers/shop.controller.ts
+++ services/api-gateway/src/controllers/shop.controller.ts
@@ -1,9 +1,30 @@
 import type { Request, Response } from "express";
+import { prisma } from "@repo/database";
 
 
 export const createShop=async(req: Request, res: Response)=>{
-    
-    res.status(201).json({ message: 'Shop created successfully' });
+    const {name, address, userId} = req.body;
+
+    // make an entry in shop table to register shop
+    const shop = await prisma.Shop.create({
+        data: {
+            name: name,
+            address: address
+        }
+    })
+
+    console.log(shop);
+
+    // make an entry in ShopUser table to register owner 
+    // const owner = await prisma.ShopUser.create({
+    //         data: {
+    //             userId: userId,
+    //             shopId: shop.id,
+    //             role: 'OWNER'    // TODO : import enum ShopUser type and add enum
+    //         }
+    // })
+
+    res.status(201).json({ message: 'Shop created successfully', data: shop });
 }
 
 export const getShopById=async(req: Request, res: Response)=>{
